0a3c16be8556390f534ae269f47020060acf0e37
Leah Philpot <leahphilpot@outlook.com>
2023-10-09 21:01:49 +0100

    started on retargeting

--- Source/ALSV4_CPP/Private/Character/Animation/ALSCharacterAnimInstance.cpp
+++ Source/ALSV4_CPP/Private/Character/Animation/ALSCharacterAnimInstance.cpp
@@ -66,7 +66,7 @@ void UALSCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
 	Super::NativeUpdateAnimation(DeltaSeconds);
 
-	if (!Character || DeltaSeconds == 0.0f || Character->GetCharacterMovement() == NULL)
+	if (!Character || DeltaSeconds == 0.0f )
 	{
 		return;
 	}
